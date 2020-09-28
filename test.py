31df1cfef4def3e5203277225ae84d2b7054a146
Denis Lunev <denis.lunev@bilsoft.dev>
2020-09-28 17:18:33 +0300

    some passes tests

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
-def sec_smallest(numbers):
-    sec_smallest_number = numbers[0]
-    for number in numbers:
-        if number <= sec_smallest_number:
-            sec_smallest_number = number
-    return sec_smallest_number
+def prime_nums(n):
+    prime_numbers = list((2,))
+    for outer_number in range(0, n):
+        is_prime = False
+        if outer_number > 1:
+            for inner_number in range(min(prime_numbers), outer_number):
+                if(outer_number % inner_number) == 0:
+                    is_prime = False
+                    break
+                else:
+                    is_prime = True
+        if is_prime:
+            prime_numbers.append(outer_number)
+    return prime_numbers
 
-num_list = [1, 2, -8, -8, -2, 0]
-print(sec_smallest(num_list))
+print(prime_nums(30))
