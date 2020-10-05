4ec767ff5cd860fba8f54684415dd16f7c89e316
Denis Lunev <denis.lunev@bilsoft.dev>
2020-10-06 01:16:35 +0300

    solved some more tasks

--- test.py
+++ test.py
@@ -1,16 +1,12 @@
-def prime_nums(n):
-    prime_numbers = list((2,))
-    for outer_number in range(0, n):
-        is_prime = False
-        if outer_number > 1:
-            for inner_number in range(min(prime_numbers), outer_number):
-                if(outer_number % inner_number) == 0:
-                    is_prime = False
-                    break
-                else:
-                    is_prime = True
-        if is_prime:
-            prime_numbers.append(outer_number)
-    return prime_numbers
+def recursive_list_sum(data_list):
+    total_sum = 0
+    for list_part in data_list:
+        if(type(list_part) is type(list())):
+            total_sum += recursive_list_sum(list_part)
+        else:
+            total_sum += list_part
+    return total_sum
 
-print(prime_nums(30))
+
+
+print('The sum of a list is ', recursive_list_sum([1, 2, [3, 4], [5, 6], [7, 8, 9, [10]]]))
