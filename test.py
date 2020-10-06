a94428f41f2122d80a124a33e24a317e16c5b130
Denis Lunev <denis.lunev@bilsoft.dev>
2020-10-06 12:06:30 +0300

    done up to 9 task

--- test.py
+++ test.py
@@ -1,12 +1,34 @@
-def recursive_list_sum(data_list):
-    total_sum = 0
-    for list_part in data_list:
-        if(type(list_part) is type(list())):
-            total_sum += recursive_list_sum(list_part)
-        else:
-            total_sum += list_part
-    return total_sum
+class Conv:
+    def __init__(self):
+        self.val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        ]
+
+        self.syb = [
+            'M', 'CM', 'D', 'CD',
+            'C', 'XC', 'L', 'XL',
+            'X', 'IX', 'V', 'IV',
+            'I'
+        ]
+        
+    def to_roman(self, num):    
+        """
+        :param self:
+        :param n: int
+        :return: str    
+        """
+        first_part_index = 0
+        first_part = 0
+        for val_item in self.val:
+            if val_item - num < 0 and val_item > first_part:
+                first_part = val_item
+                first_part_index = self.val.index(val_item)
+        second_part = num - first_part
+        second_part_index = self.val.index(second_part)
+        return f'{self.syb[first_part_index]}{self.syb[second_part_index]}'
 
 
 
-print('The sum of a list is ', recursive_list_sum([1, 2, [3, 4], [5, 6], [7, 8, 9, [10]]]))
+print('Converted:', Conv().to_roman(44))
